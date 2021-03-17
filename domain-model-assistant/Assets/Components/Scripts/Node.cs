82feb522ac3107a197ff58f283f8f21dbd2d5a6f
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-03-17 14:53:24 -0400

    Added node clas with UMPLE-like methods

--- domain-model-assistant/Assets/Components/Scripts/Node.cs
+++ domain-model-assistant/Assets/Components/Scripts/Node.cs
@@ -1,52 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Node: MonoBehaviour, BaseComponent {
-    // private string id;
-    public GameObject canvas;
-    // renamed textbox as header to avoid parent and child
-    // from having the same serializable field
-    public GameObject header;
-    public string ID{
-        get{
-            return ID;
-        }
-        set{
-            ID = value;
-        }
+
+    private String id;
+    private Canvas canvas;
+    private Textbox header;
+    private List<Node> composites;
+    private List<Edge> connections;
+
+    public String getId(){
+        return id;
     }
 
-    public GameObject getCanvas(){
+    public boolean setId(int id){
+        boolean wasSet = false;
+        this.id = id;
+        wasSet = true;
+        return wasSet; 
+
+    }
+
+    public Canvas getCanvas(){
         return canvas;
     }
 
-    public bool setCanvas(GameObject aCanvas){
-        bool wasSet = false;
+    public boolean setCanvas(Canvas aCanvas){
+        boolean wasSet = false;
         if(aCanvas == null){
             return wasSet;
         }
         canvas = aCanvas;
-        Debug.Log("Canvas has been set for Node");
+        canvas.addNode(this);
         wasSet = true;
         return wasSet;
     }
 
-    public GameObject getHeader(){
+    public Textbox getTextbox(){
         return header;
     }
-
-    public bool addHeader(GameObject aheader){
-        bool wasSet = false;
+    public boolean setTextbox(Texbox aheader){
+        boolean wasSet = false;
         if(aheader == null){
             return wasSet;
         }
         header = aheader;
-        Debug.Log("Header textbox has been set for Node");
+        header.addNode(this);
+        wasSet = true;
+        return wasSet;
+    }
+    public Node getNode(int index){
+        Node aNode = composites[index];
+        return aNode;
+    }
+
+    public List<Node> getNodes(){
+        List<Node> aComposites = Collections.unmodifiableList(composites);
+        return aComposites;
+    }
+
+    public boolean addNode(Node aNode){
+        boolean wasSet = false;
+        if(composites.contains(aNode)){
+            return false;
+        }
+        composites.add(aNode);
         wasSet = true;
         return wasSet;
     }
 
-
+    public Edge getEdge(int index){
+        Edge aEdge = connections[index];
+        return aEdge; 
+    }
+    public List<Edge> getEdges(){
+        List<Edge> aEdges = Collections.unmodifiableList(connections);
+        return aEdges;  
+    }
+    public boolean addEdge(Edge aEdge){
+        boolean wasSet = false;
+        if(connections.contains(aEdge)){
+            return wasSet;
+        }
+        connections.add(aEdge);
+        wasSet = true;
+        return wasSet;
+    }
 }
--- domain-model-assistant/Assets/Scripts/Canvas.cs
+++ domain-model-assistant/Assets/Scripts/Canvas.cs
@@ -3,32 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+public class Canvas : MonoBehaviour, IPointerClickHandler
+{
 
-public class Canvas : MonoBehaviour{
-
-    public TextAsset jsonFile;
+    public GameObject compartmentedRectangle;
+    public List<GameObject> compRectList;
     public float zoomSpeed = 1;
     public CanvasScaler CanvasScaler;
     public float targetOrtho;
     public float smoothSpeed = 2.0f;
     public float minOrtho = 0.0f;
     public float maxOrtho = 20.0f;
+    public GameObject classDiagramPrefeb;
+    public List<GameObject> classDiagramList;
     private Vector3 dragStartPos;
-    // private bool dragging = false;
-    public GameObject compartmentedRectangle;
-    public List<GameObject> compRectList;
-    private ClassDiagramDTO classDTO;
-    public string ID{
-        get{
-            return ID;
-        }
-        set{
-            ID = value;
-        }
-    }
-    void Awake(){
-         LoadData();
-    }
+    private bool dragging = false;
+    private PointerEventData ped = new PointerEventData(null);
     // Start is called before the first frame update
     GraphicRaycaster raycaster;
     void Start()
@@ -36,6 +26,7 @@ public class Canvas : MonoBehaviour{
         CanvasScaler = this.gameObject.GetComponent<CanvasScaler>();
         targetOrtho = CanvasScaler.scaleFactor;
         this.raycaster = GetComponent<GraphicRaycaster>();
+
     }
 
     // Update is called once per frame
@@ -44,89 +35,79 @@ public class Canvas : MonoBehaviour{
         if(InputExtender.MouseExtender.isDoubleClick(0))
         {
             Vector2 tempFingerPos = (Input.mousePosition);
-            CreateCompartmentedRectangle(tempFingerPos);
+            CreateClassDiagram(tempFingerPos);
+            // CreateCompartmentedRectangle(tempFingerPos);
         }
         Zoom();
     }
 
-
-
-
-// ************ Controller Methods for Canvas/Diagram ****************//
-   private void LoadData(){
-        Debug.Log("Loading data ...");
-        // obtain class DTO from json string format
-        classDTO = JsonUtility.FromJson<ClassDiagramDTO>(jsonFile.text);
-        // convert float positions to Vector2
-        Vector2 position = new Vector2(classDTO.layout.containers[0].value[0].value.x, 
-                                        classDTO.layout.containers[0].value[0].value.y);
-        // create comp rectangle with header and sections
-        GameObject newCompRect = CreateCompartmentedRectangle(position);
-        // set the header value of the created class
-        newCompRect.GetComponent<CompartmentedRectangle>().getHeader().
-                    GetComponent<TextBox>().setText(classDTO.classes[0].name);
-    }
-    
-    public GameObject CreateCompartmentedRectangle(Vector2 position)
+    public GameObject CreateClassDiagram(Vector2 position)
     {
-        GameObject compRect = Instantiate(compartmentedRectangle, this.transform);
-        compRect.transform.position = position;
-        addNode(compRect);
-        return compRect;
+        GameObject classDiagram = Instantiate(classDiagramPrefeb, this.transform);
+        classDiagram.transform.position = position;
+        classDiagramList.Add(classDiagramPrefeb);
+        return classDiagram;
     }
 
-    void Zoom(){
-        if (Input.touchSupported){
-            if (Input.touchCount == 2){
+    public GameObject CreateClassDiagram(Vector2 position, GameObject prefab)
+    {
+        GameObject diagram = Instantiate(prefab, this.transform);
+        diagram.transform.position = position;
+        classDiagramList.Add(diagram);
+        return diagram;
+    }
+
+    public GameObject DeleteClassDiagram(Vector2 position, GameObject prefab)
+    {
+        GameObject diagram = Instantiate(prefab, this.transform);
+        diagram.transform.position = position;
+        classDiagramList.Add(diagram);
+        return diagram;
+    }
+
+    void Zoom()
+    {
+        if (Input.touchSupported)
+        {
+            if (Input.touchCount == 2)
+            {
                 // get current touch positions
                 Touch tZero = Input.GetTouch(0);
                 Touch tOne = Input.GetTouch(1);
                 // get touch position from the previous frame
                 Vector2 tZeroPrevious = tZero.position - tZero.deltaPosition;
                 Vector2 tOnePrevious = tOne.position - tOne.deltaPosition;
+
                 float oldTouchDistance = Vector2.Distance (tZeroPrevious, tOnePrevious);
                 float currentTouchDistance = Vector2.Distance (tZero.position, tOne.position);
-                if ((oldTouchDistance - currentTouchDistance) != 0.0f){
+
+                
+                if ((oldTouchDistance - currentTouchDistance) != 0.0f)
+                {
                     targetOrtho += Mathf.Clamp ((oldTouchDistance - currentTouchDistance), -1, 1) * zoomSpeed * 0.03f ;
                     targetOrtho = Mathf.Clamp (targetOrtho, minOrtho, maxOrtho);
                 }
             }
         }
-        else{
+        else
+        {
             float scroll = Input.GetAxis ("Mouse ScrollWheel");
-            if (scroll != 0.0f) {
+            if (scroll != 0.0f) 
+            {
                 targetOrtho += scroll * zoomSpeed * 0.3f;
                 targetOrtho = Mathf.Clamp (targetOrtho, minOrtho, maxOrtho);
             }
         }
+        
         CanvasScaler.scaleFactor = Mathf.MoveTowards(CanvasScaler.scaleFactor, targetOrtho, smoothSpeed * Time.deltaTime);
     }
 
 
 
-
-// ************ UI model Methods for Canvas/Diagram ****************//
-    public bool addNode(GameObject aNode){
-        bool wasSet = false;
-        if(compRectList.Contains(aNode)){
-            return false;
-        }
-        compRectList.Add(aNode);
-        aNode.GetComponent<CompartmentedRectangle>().setCanvas(this.gameObject);
-        Debug.Log("Node added to list of compartmented rectangles");
-        wasSet = true;
-        return wasSet;
+    public void OnPointerClick(PointerEventData data)
+    {
+        Debug.Log("");
     }
-
-    public List<GameObject> getCompartmentedRectangles(){
-            return compRectList;
-        }
-
-
-
-
-
-
 
 
 
