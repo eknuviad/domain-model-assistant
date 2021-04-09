945557866642a3236900d775ee3279572561f23a
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-04-08 21:33:20 -0400

    Hot Fix after horrible demo

--- domain-model-assistant/Assets/Components/Scripts/DTO/ClassDiagramDTO.cs
+++ domain-model-assistant/Assets/Components/Scripts/DTO/ClassDiagramDTO.cs
@@ -4,7 +4,51 @@ using UnityEngine;
 
 [System.Serializable]
 public class ClassDiagramDTO {
+    public string eClass;
+    public string _id;
     public string name;
+    public Classes[] classes;
+    public Types[] types;
+    public Layout layout;
+};
+
+
+[System.Serializable]
+public class Classes{
+    public string eClass;
+    public string _id;
+    public string name;
+};
+
+[System.Serializable]
+public class Types{
+    public string eClass;
+    public string _id;
+};
+
+[System.Serializable]
+public class Layout{
+    public string _id;
+    public Containers[] containers;
+};
+
+[System.Serializable]
+public class Containers{
+    public string _id;
+    public string key;
+    public Value[] value;
+};
+
+[System.Serializable]
+public class Value{
+    public string _id;
+    public string x;
+    public Cordinate value;
+};
+
+[System.Serializable]
+public class Cordinate{
+    public string _id;
     public float x;
     public float y;
-}
+};
