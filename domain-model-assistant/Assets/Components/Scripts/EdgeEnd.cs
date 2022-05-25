80df8ab303a0437793b05703c2d89586967828ac
Linwei Yuan <56415387+greyyuan211@users.noreply.github.com>
2022-05-25 00:35:31 -0400

    Revert "Merge pull request #111 from eknuviad/revert-110-development-branch"
    This reverts commit 3a8659e022f2604f151cd7f213cb53f0df18b5e4, reversing
    changes made to a193a5c0aa45e461c67cd508651529297ef9daaf.

--- domain-model-assistant/Assets/Components/Scripts/EdgeEnd.cs
+++ domain-model-assistant/Assets/Components/Scripts/EdgeEnd.cs
@@ -13,28 +13,183 @@ public class EdgeEnd : MonoBehaviour
     public string ID
     { get; set; }
     
+    public Vector2 Position
+    { get; set; }
+
     public GameObject edgeEndTitle;
     public GameObject edgeEndNumber;
     public GameObject textbox;
+
+    public bool isUpper = true;
+    public bool isLeft = true;
+    public static Vector2 TitleVerticalOffset = new Vector2(0,40);
+    public static Vector2 TitleHorizontalOffset = new Vector2(80,0);
+    public static Vector2 NumberTitleOffset = new Vector2(0,-20);
+    private GameObject edge;
+    public GameObject compositionIcon;
+    public GameObject aggregationIcon;
+    public GameObject generalizationIcon;
+    public bool hasActiveIcon = false;
     // public GameObject popupMenu;
     void Start()
     {
-        // this.gameObject.transform.SetParent(GameObject.Find("Canvas").transform);
-        // edgeEndTitle = GameObject.Instantiate(textbox, this.transform);
+        this.gameObject.transform.SetParent(GameObject.Find("Canvas").transform);
+        edgeEndTitle = GameObject.Instantiate(textbox, this.transform);
         edgeEndTitle.GetComponent<InputField>().text = "Enter Text ...";
-        // edgeEndNumber = GameObject.Instantiate(textbox, this.transform);
-        // edgeEndNumber.transform.position += new Vector3(0,20,0);
+        edgeEndTitle.transform.position = Position + new Vector2(0,40);
+        edgeEndNumber = GameObject.Instantiate(textbox, this.transform);
+        edgeEndNumber.GetComponent<InputField>().text = "*";
+        edgeEndNumber.transform.position = Position + new Vector2(85,-20);
+
+        generalizationIcon = GameObject.Instantiate(generalizationIcon);
+        aggregationIcon = GameObject.Instantiate(aggregationIcon);
+        compositionIcon = GameObject.Instantiate(compositionIcon);
+
+        generalizationIcon.SetActive(false);
+        aggregationIcon.SetActive(false);
+        compositionIcon.SetActive(false);
+
+        generalizationIcon.GetComponent<GeneralizationIcon>().SetEdgeEnd(this);
+        aggregationIcon.GetComponent<AggregationIcon>().SetEdgeEnd(this);
+        compositionIcon.GetComponent<CompositionIcon>().SetEdgeEnd(this);
+
         Debug.Log("instatntiate");
     }
 
     void Update()
     {
-     
+        Vector2 titlePos;
+        Vector2 numberPos;
+
+        if (isUpper)
+        {
+            if (isLeft)
+            {
+                titlePos = Position - TitleVerticalOffset;
+            }
+            else
+            {
+                titlePos = Position - TitleVerticalOffset - TitleHorizontalOffset;
+            }
+        }
+        else
+        {
+            if(isLeft)
+            {
+                titlePos = Position + TitleVerticalOffset;
+            }
+            else
+            {
+                titlePos = Position + TitleVerticalOffset - TitleHorizontalOffset;
+            }
+        }
+
+        numberPos = titlePos + NumberTitleOffset;
+
+        edgeEndTitle.transform.position = titlePos;
+        edgeEndNumber.transform.position = numberPos;
+
+        compositionIcon.transform.position = Position;
+        aggregationIcon.transform.position = Position;
+        generalizationIcon.transform.position = Position;
     }
 
-
-    void Destroy(){
+    void Destroy()
+    {
          Destroy(this.gameObject);
+    }
+
+    public GameObject GetEdge()
+    {
+        return edge;
+    }
+
+    public bool SetEdge(GameObject aEdge)
+    {
+        bool wasSet = false;
+        if (aEdge == null)
+        {
+            return wasSet;
+        }
+        Edge edgeComp = aEdge.GetComponent<Edge>();
+        // Check if edge already have 2 or more edge ends 
+        if (edgeComp.GetNumberOfEdgeEnds() >= Edge.RequiredNumOfEdgeEnds)
+        {
+            return wasSet;
+        }
+        // Check if edge has already been set
+        if (edge != null)
+        {
+            return wasSet;
+        }
+        edge = aEdge;
+        edge.GetComponent<Edge>().AddEdgeEnd(this.gameObject);
+        wasSet = true;
+        Debug.Log("Edge end: Edge set");
+        return wasSet;
+    }
+
+    public void SetIconType(int type)
+    {
+        switch (type)
+        {
+            case 0:
+                //TODO  
+               //ASSOCIATION - same as line, 
+               //should destroy any existing icon at intended location
+                compositionIcon.SetActive(false);
+                generalizationIcon.SetActive(false);
+                aggregationIcon.SetActive(false);
+                hasActiveIcon = false;
+                break;
+            case 1:
+                hasActiveIcon = true;
+                if (compositionIcon.activeSelf==true)
+                {
+                    compositionIcon.SetActive(false);
+                }
+                if (generalizationIcon.activeSelf==true)
+                {
+                    generalizationIcon.SetActive(false);
+                }
+                if (aggregationIcon.activeSelf==false)
+                {
+                    aggregationIcon.SetActive(true);
+                }
+                break;
+            case 2:
+                hasActiveIcon = true;
+                if (generalizationIcon.activeSelf==true)
+                {
+                    generalizationIcon.SetActive(false);
+                }
+                if (aggregationIcon.activeSelf==true)
+                {
+                    aggregationIcon.SetActive(false);
+                }
+                if (compositionIcon.activeSelf==false)
+                {
+                    compositionIcon.SetActive(true);
+                }
+                break;
+             case 3:
+                hasActiveIcon = true;
+                if (compositionIcon.activeSelf==true)
+                {
+                    compositionIcon.SetActive(false);
+                }
+                if (aggregationIcon.activeSelf==true)
+                {
+                    aggregationIcon.SetActive(false);
+                }
+                if (generalizationIcon.activeSelf==false)
+                {
+                    generalizationIcon.SetActive(true);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
 }
---
+++ domain-model-assistant/Assets/Components/Scripts/LineCollider.cs
@@ -0,0 +1,118 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script instantiates a polygonCollider2D with the same shape as the line renderer 
+/// object in edge, and move along with the edge. It detects mouse hovering, mouse exit and mouse clicks activities which allows
+/// highlighting upon left click and linePopupMenu spawning upon right click.
+/// </summary>
+[RequireComponent(typeof(Edge), typeof(PolygonCollider2D))]
+public class LineCollider : MonoBehaviour
+{
+
+    Edge edge;
+
+    //The collider for the line
+    PolygonCollider2D polygonCollider2D;
+
+    LineRenderer highlightBox;
+
+    Boolean isSelected = false;
+
+
+    //The points to draw a collision shape between
+    List<Vector2> colliderPoints = new List<Vector2>(); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        edge = GetComponent<Edge>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        colliderPoints = CalculateColliderPoints();
+        polygonCollider2D.SetPath(0, colliderPoints.ConvertAll(p => (Vector2)transform.InverseTransformPoint(p)));
+    }
+
+    
+    //mouse hover, not clicked, set color to blue
+    void OnMouseEnter()
+    {  
+        if (!isSelected)
+        {
+            edge.setColor(1);
+        }
+    }
+    //mouse exits, set color back to black
+    void OnMouseExit()
+    {
+        if(!isSelected){
+            edge.setColor(0);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (isSelected)
+        {
+            isSelected = false;
+            edge.setColor(1);
+        }
+        else
+        {
+            edge.setColor(2);
+            isSelected = true;
+        }
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            edge.mousePos = Input.mousePosition;
+            edge.SpawnPopupLineMenu();
+        }
+    }
+    
+    private List<Vector2> CalculateColliderPoints() 
+    {
+        //Get All positions on the line renderer
+        Vector3 linePos1 = edge.GetPosition1();
+        Vector3 linePos2 = edge.GetPosition2();
+
+        //Get the Width of the Line
+        // float width = 0.5f;
+        float width = 2f * edge.GetWidth();
+
+        //m = (y2 - y1) / (x2 - x1)
+        //slope for vertical lines are undefined
+        float slope = linePos2.x - linePos1.x !=0 ? (linePos2.y - linePos1.y) / (linePos2.x - linePos1.x) : 0;
+        float deltaX = (width / 2f) * (slope / Mathf.Pow(slope * slope + 1, 0.5f));
+        float deltaY = (width / 2f) * (1 / Mathf.Pow(1 + slope * slope, 0.5f));
+
+        //Calculate the Offset from each point to the collision vertex
+        Vector3[] offsets = new Vector3[2];
+        offsets[0] = new Vector3(-deltaX, deltaY);
+        offsets[1] = new Vector3(deltaX, -deltaY);
+
+        //Generate the Colliders Vertices
+        var colliderPositions = new List<Vector2> {
+            linePos1 + offsets[0],
+            linePos2 + offsets[0],
+            linePos2 + offsets[1],
+            linePos1 + offsets[1]
+        };
+
+        return colliderPositions;
+    }
+    
+}
