9241ce11b3de427c402d4210a7f562b0612282df
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-08-04 00:56:48 -0400

    Add automated deminification of Unity Framework JS file
    Needed to debug frontend

--- deminify-unity-framework.js
+++ deminify-unity-framework.js
@@ -3,15 +3,23 @@
 const exec = require('child_process');
 const fs = require('fs');
 
+const beautify = require('js-beautify').js; // from Beautifier.io
+
 const unityFrameworkJsFile = "unity-webgl-output/Build/unity-webgl-output.framework.js";
 const unityFrameworkGzFile = `${unityFrameworkJsFile}.gz`;
+
+const beautifyOptions = {
+  indent_size: 2,
+  wrap_line_length: 120,
+  end_with_newline: true,
+};
 
 console.log("Deminifying Unity WebGL Framework JS file...");
 
 // Unzip GZ file
 exec.execSync(`gunzip -kf ${unityFrameworkGzFile}`);
 
-// Read Unity Framework JS file
+// Read Unity Framework JS file, deminify it, write it back to the file, and rezip it
 fs.readFile(unityFrameworkJsFile, (err, data) => {
   if (err) {
     console.error(err);
@@ -27,14 +35,21 @@ fs.readFile(unityFrameworkJsFile, (err, data) => {
   });
 });
 
+/**
+ * Deminify the input JS source code.
+ * 
+ * @param {string} jsSource 
+ * @returns the deminified JS source
+ */
 function deminify(jsSource) {
-  return (jsSource
-      .replaceAll(";if(", ";\n  if (")
-      .replaceAll("}if(", "}\n  if (")
-      .replaceAll(";for(", ";\n  for (")
-      .replaceAll("}for(", "}\n  for (")
-      .replaceAll(";var ", ";\n  var ")
-      .replaceAll("}var ", "}\n  var ")
-  );
+  // break up source into multiple lines for faster processing
+  jsSource = (jsSource
+    .replaceAll(";if(", ";\n  if (")
+    .replaceAll("}if(", "}\n  if (")
+    .replaceAll(";for(", ";\n  for (")
+    .replaceAll("}for(", "}\n  for (")
+    .replaceAll(";var ", ";\n  var ")
+    .replaceAll("}var ", "}\n  var "));
+  return beautify(jsSource, beautifyOptions);
 }
 
