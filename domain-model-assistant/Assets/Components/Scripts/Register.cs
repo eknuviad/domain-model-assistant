a22c130af86e7c92520ce1c4f613cbc1d1fe48d1
Linwei Yuan <56415387+greyyuan211@users.noreply.github.com>
2022-11-12 22:26:12 -0500

    register user

---
+++ domain-model-assistant/Assets/Components/Scripts/Register.cs
@@ -0,0 +1,29 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Register : MonoBehaviour
+{
+    public TextMeshProUGUI errorlog;
+    public TMP_InputField userInputField;
+    public TMP_InputField emailInputField;
+    public TMP_InputField passwordInputField;
+    public TMP_InputField confirmedPasswordInputField;
+    public void RegisterUser()
+    {
+        if (passwordInputField.text == confirmedPasswordInputField.text)
+        {
+            User user = new User(userInputField.text, passwordInputField.text);
+        }
+        else
+        {
+            errorlog.text = "Passwords do not match";
+        }
+        Debug.Log(userInputField.text);
+        Debug.Log(emailInputField.text);
+        Debug.Log(passwordInputField.text);
+        Debug.Log(confirmedPasswordInputField.text);
+    }
+}
